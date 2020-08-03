60da20755c239a0224aa45456d8bd2450477d71a
Yuki Shibata <yuki.shibata58@gmail.com>
2020-08-03 11:07:37 +0900

    [add] Add UnityProjectFolder
    ・プロジェクトの初期のバージョンをリポジトリに統合しました。

---
+++ AnotherDeleter/Assets/Scripts/ChasePlayer.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ChasePlayer : MonoBehaviour
+{
+    [SerializeField]
+    GameObject player = default;
+    [SerializeField]
+    Vector3 offsetFromPlayerPos = new Vector3(0, -5, 2);
+    // Start is called before the first frame update
+    void Start()
+    {
+        transform.position = player.transform.position - offsetFromPlayerPos;
+       
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.position = player.transform.position - offsetFromPlayerPos;
+        LookPlayer();
+    }
+
+    private void LookPlayer()
+    {
+        Vector3 cameraToPlayerVec = player.transform.position - this.transform.position;
+        //Debug.DrawLine(player.transform.position, transform.position,Color.red);
+        transform.localRotation = Quaternion.LookRotation(cameraToPlayerVec);
+    }
+}
