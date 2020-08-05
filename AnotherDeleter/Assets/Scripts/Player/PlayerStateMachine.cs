a5bab60de328c84fa2f04ffac18962efda49bf35
Yuki Shibata <yuki.shibata58@gmail.com>
2020-08-05 16:05:41 +0900

    [update] Update StateMachine
    ・　StateMachine 及び　State の設計を更新しました
    ・　StateMachine 及び　State　のインターフェースを追加しました
    ・　上記に伴ってプレイヤー用のステート及びステートマシンも更新しました

---
+++ AnotherDeleter/Assets/Scripts/Player/PlayerStateMachine.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System;
+
+namespace AnotherDeleter.GameScene {
+
+    /// <summary>
+    /// プレイヤー用ステートマシン
+    /// </summary>
+    [Serializable]
+    public class PlayerStateMachine : StateMachine {
+        // 各ステートのインスタンス    
+        [SerializeField]
+        readonly PlayerIdleState idleState = new PlayerIdleState();
+        [SerializeField]
+        readonly PlayerMoveState moveState = new PlayerMoveState();
+
+        /// <summary>
+        /// MonovihaberのStartで呼んでほしい初期化処理
+        /// </summary>
+        /// <param name="target">ステートの対象オブジェクト</param>
+        public override void Start(GameObject target){
+            // ステートの登録
+            AddState<PlayerIdleState>(idleState, target);
+            AddState<PlayerMoveState>(moveState, target);
+            // 初期ステートをセット
+            ChangeState(typeof(PlayerIdleState));
+        }
+
+    }
+}
+
--- AnotherDeleter/Assets/Scripts/SceneController.cs
+++ AnotherDeleter/Assets/Scripts/SceneController.cs
@@ -2,25 +2,8 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class SceneController : MonoBehaviour
-{
-    [SerializeField]
-    PlayerStateMachine playerStateMachine = null;
-
-    private void Awake() {
-        playerStateMachine.Init();
-    }
-    // Start is called before the first frame update
-    void Start(){
-        
-    }
-
-    // Update is called once per frame
-    void Update(){
-        playerStateMachine.Do();
-    }
-
-    private void FixedUpdate() {
-        playerStateMachine.Do_Fix();
+namespace AnotherDeleter.GameScene {
+    public class SceneController : MonoBehaviour {
     }
 }
+
